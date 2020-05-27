de9629614c65ba41b5219363cba2cf657804010f
Nikhita <nikhita.garg@globallogic.com>
2020-05-27 17:23:21 +0530

    latest CommonStepDefs.java from master

--- src/test/java/stepDefinitions/CommonStepDefs.java
+++ src/test/java/stepDefinitions/CommonStepDefs.java
@@ -38,17 +38,11 @@ public class CommonStepDefs extends TestBase {
         else if(splitEndpoint[0].toUpperCase().equalsIgnoreCase("PET")){
             URI= PetEndpoint.valueOf(splitEndpoint[1]).getResource();
         }
-        else if(splitEndpoint[0].toUpperCase().equalsIgnoreCase("QUOTE")) {
-            URI = quoteEndPoint.valueOf(splitEndpoint[1]).getResource();
-        }
         else if(splitEndpoint[0].toUpperCase().equalsIgnoreCase("CART")) {
             URI = CartEndpoint.valueOf(splitEndpoint[1]).getResource();
         }
         else if(splitEndpoint[0].toUpperCase().equalsIgnoreCase("LOOKUP")){
             URI= ZipCode.valueOf(splitEndpoint[1]).getResource();
-        }
-        else if(splitEndpoint[0].toUpperCase().equalsIgnoreCase("CART")) {
-            URI = CartEndpoint.valueOf(splitEndpoint[1]).getResource();
         }
         PropertyHolder.setProperty("URI",URI);
         log.info(URI);
