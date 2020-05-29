7fbba11740688eae41e7a64e2a0c3f50896a5299
Nikhita <nikhita.garg@globallogic.com>
2020-05-29 14:53:45 +0530

    Updated scripts for Refactored story FDP-816 of FDP-397

--- src/test/java/model/Request/Pet/CreatePetProspect/Weight.java
+++ src/test/java/model/Request/Pet/CreatePetProspect/Weight.java
@@ -1,25 +1,55 @@
 
 package model.Request.Pet.CreatePetProspect;
 
+
 public class Weight {
 
     private String date;
     private String uom;
     private Double weight;
 
-    public Weight(String date,String uom,Double weight){
-        this.date=date;
-        this.uom=uom;
-        this.weight=weight;
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public Weight() {
+    }
+
+    /**
+     * 
+     * @param date
+     * @param uom
+     * @param weight
+     */
+    public Weight(String date, String uom, Double weight) {
+        super();
+        this.date = date;
+        this.uom = uom;
+        this.weight = weight;
     }
 
     public String getDate() {
         return date;
     }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
     public String getUom() {
         return uom;
     }
+
+    public void setUom(String uom) {
+        this.uom = uom;
+    }
+
     public Double getWeight() {
         return weight;
     }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
 }
